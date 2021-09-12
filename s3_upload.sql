c693c091beb024cbdb1b6891219ba4303efb7a0a
Anil <maramanil.rko@gmail.com>
2021-09-12 13:17:43 +0530

    Update s3_upload.sql

--- s3_upload.sql
+++ s3_upload.sql
@@ -2,7 +2,7 @@ set head off;
 set feed  off;
 spool /home/orards/AAScripts/task_value.txt
 SELECT rdsadmin.rdsadmin_s3_tasks.upload_to_s3(
-p_bucket_name => 's3-bucket',
+p_bucket_name => 's3-bucket-name',
 p_prefix => 'SCHEMA.DMP',
 p_s3_prefix => 'BACKUP/',
 p_directory_name => 'DATA_PUMP_DIR')
