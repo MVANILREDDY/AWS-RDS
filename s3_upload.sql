6e52ae12afaf2dbc9c75df4617fb6f5cf01fb2eb
Anil <maramanil.rko@gmail.com>
2021-09-12 13:17:09 +0530

    Create s3_upload.sql

---
+++ s3_upload.sql
@@ -0,0 +1,9 @@
+set head off;
+set feed  off;
+spool /home/orards/AAScripts/task_value.txt
+SELECT rdsadmin.rdsadmin_s3_tasks.upload_to_s3(
+p_bucket_name => 's3-bucket',
+p_prefix => 'SCHEMA.DMP',
+p_s3_prefix => 'BACKUP/',
+p_directory_name => 'DATA_PUMP_DIR')
+AS TASK_ID FROM DUAL;
